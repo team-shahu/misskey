9065d92f9fefdcb6b461178136d21644d0e2a143
n1lsqn <owner@n1l.dev>
2024-01-14 22:30:10 +0900

    feat: もっとやナビゲーションバーに追加できるようにする

--- packages/frontend/src/navbar.ts
+++ packages/frontend/src/navbar.ts
@@ -4,6 +4,7 @@
  */
 
 import { computed, reactive } from 'vue';
+import { clearCache } from './scripts/clear-cache.js';
 import { $i } from '@/account.js';
 import { miLocalStorage } from '@/local-storage.js';
 import { openInstanceMenu, openToolsMenu } from '@/ui/_common_/common.js';
@@ -12,7 +13,6 @@ import * as os from '@/os.js';
 import { i18n } from '@/i18n.js';
 import { ui } from '@/config.js';
 import { unisonReload } from '@/scripts/unison-reload.js';
-import { clearCache } from './scripts/clear-cache.js';
 
 export const navbarItemDef = reactive({
 	notifications: {
@@ -178,5 +178,10 @@ export const navbarItemDef = reactive({
 		action: (ev) => {
 			clearCache();
 		},
+	},
+	bubbleGame: {
+		title: i18n.ts.bubbleGame,
+		icon: 'ti ti-apple',
+		to: '/bubble-game',
 	},
 });

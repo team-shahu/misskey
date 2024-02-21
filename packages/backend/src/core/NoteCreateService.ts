5e1aa22a00f1415e5a7ccc931dfd05bb33d9adb7
n1lsqn <owner@n1l.dev>
2024-02-21 10:58:02 +0900

    fix: セミコロン

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -361,7 +361,7 @@ export class NoteCreateService implements OnApplicationShutdown {
 
 		// #region *.n1l.dev
 		// ローカル宛てのメンション、リプライの発行元が、ローカルユーザーにフォローされていない場合は投稿を拒否する
-		const willCauseNotification = mentionedUsers.filter(u => u.host === null).length > 0 || (data.reply != null && data.reply.userHost === null)
+		const willCauseNotification = mentionedUsers.filter(u => u.host === null).length > 0 || (data.reply != null && data.reply.userHost === null);
 
 		const instance = await this.metaService.fetch(true);
 
